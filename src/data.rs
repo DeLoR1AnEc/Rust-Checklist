f96b7cebcdaf9986768ecca2180a8a0ca74923ce
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2025-07-09 18:39:40 +0200

    stupid commit

--- src/data.rs
+++ src/data.rs
@@ -7,13 +7,13 @@ use uuid::Uuid;
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 // --- Meta ---
-pub struct StructMeta {
+struct NodeMeta {
   id: Uuid,
   name: String,
   desc: String,
 }
 
-impl StructMeta {
+impl NodeMeta {
   fn new(name: String, desc: String) -> Self {
     Self {
       id: Uuid::new_v4(),
@@ -21,94 +21,77 @@ impl StructMeta {
       desc: desc,
     }
   }
-
-  fn id(&self) -> &Uuid {
-    &self.id
-  }
-
-  fn name(&self) -> &str {
-    &self.name
-  }
-
-  fn set_name(&mut self, name: String) {
-    self.name = name;
-  }
-
-  fn desc(&self) -> &str {
-    &self.desc
-  }
-
-  fn set_desc(&mut self, desc: String) {
-    self.desc = desc;
-  }
 }
 
-pub trait HasMeta {
-  fn meta(&self) -> &StructMeta;
-  fn meta_mut(&mut self) -> &mut StructMeta;
-
-  fn id(&self) -> &Uuid {
-    self.meta().id()
-  }
-
-  fn name(&self) -> &str {
-    self.meta().name()
-  }
-
-  fn set_name<S: Into<String>>(&mut self, name: S) {
-    self.meta_mut().set_name(name.into());
-  }
-
-  fn desc(&self) -> &str {
-    self.meta().desc()
-  }
-
-  fn set_desc<S: Into<String>>(&mut self, desc: S) {
-    self.meta_mut().set_desc(desc.into())
-  }
-}
-
-
-// --- Data ---
-
+// Node
 pub enum Node {
-  Container(Container),
   Entry(Entry),
+  Container(Container),
 }
 
 impl Node {
-  pub fn as_container_mut(&mut self) -> Option<&mut Container> {
+  pub fn get_entry(&mut self) -> Option<&mut Entry> {
+    match self {
+      Node::Entry(e) => Some(e),
+      _ => None,
+    }
+  }
+
+  pub fn get_container(&mut self) -> Option<&mut Container> {
     match self {
       Node::Container(c) => Some(c),
       _ => None,
     }
   }
 
-  pub fn as_entry_mut(&mut self) -> Option<&mut Entry> {
+  pub fn is_entry(&self) -> bool {
+    matches!(self, Node::Entry(_))
+  }
+
+  pub fn is_container(&self) -> bool {
+    matches!(self, Node::Container(_))
+  }
+
+  // Meta
+  fn meta(&self) -> &NodeMeta {
     match self {
-      Node::Entry(e) => Some(e),
-      _ => None,
+      Node::Entry(e) => &e.meta,
+      Node::Container(c) => &c.meta,
     }
+  }
+
+  fn meta_mut(&mut self) -> &mut NodeMeta {
+    match self {
+      Node::Entry(e) => &mut e.meta,
+      Node::Container(c) => &mut c.meta,
+    }
+  }
+
+  pub fn id(&self) -> &Uuid {
+    &self.meta().id
+  }
+
+  pub fn name(&self) -> &str {
+    &self.meta().name
+  }
+
+  pub fn set_name<S: Into<String>>(&mut self, name: S) {
+    self.meta_mut().name = name.into();
+  }
+
+  pub fn desc(&self) -> &str {
+    &self.meta().desc
+  }
+
+  pub fn set_desc<S: Into<String>>(&mut self, desc: S) {
+    self.meta_mut().desc = desc.into();
   }
 }
 
-impl HasMeta for Node {
-  fn meta(&self) -> &StructMeta {
-    match self {
-      Node::Entry(e) => e.meta(),
-      Node::Container(c) => c.meta(),
-    }
-  }
+// --- Data ---
 
-  fn meta_mut(&mut self) -> &mut StructMeta {
-    match self {
-      Node::Entry(e) => e.meta_mut(),
-      Node::Container(c) => c.meta_mut(),
-    }
-  }
-}
-
-// Entry
+// Entry state
+#[derive(Clone)]
 pub enum EntryState {
   Pending,
   InProgress,
@@ -140,104 +123,223 @@ impl EntryState {
   }
 }
 
+// Entry
 pub struct Entry {
-  meta: StructMeta,
+  meta: NodeMeta,
   state: EntryState,
 }
 
 impl Entry {
   pub fn new<S: Into<String>>(name: S, desc: S) -> Self {
     Self {
-      meta: StructMeta::new(name.into(), desc.into()),
+      meta: NodeMeta::new(name.into(), desc.into()),
       state: EntryState::Pending,
     }
-  }
-
-  pub fn state(&self) -> &EntryState {
-    &self.state
-  }
-
-  pub fn set_state(&mut self, state: EntryState) {
-    self.state = state;
-  }
-
-  pub fn next_state(&mut self) {
-    self.state = self.state.next();
-  }
-
-  pub fn prev_state(&mut self) {
-    self.state = self.state.prev();
-  }
-}
-
-impl HasMeta for Entry {
-  fn meta(&self) -> &StructMeta {
-    &self.meta
-  }
-
-  fn meta_mut(&mut self) -> &mut StructMeta{
-    &mut self.meta
   }
 }
 
 // Container
 pub struct Container {
-  meta: StructMeta,
-  items: HashMap<Uuid, Node>,
+  meta: NodeMeta,
   order: Vec<Uuid>,
 }
 
 impl Container {
   pub fn new<S: Into<String>>(name: S, desc: S) -> Self {
     Self {
-      meta: StructMeta::new(name.into(), desc.into()),
-      items: HashMap::new(),
-      order: Vec::new(),
+      meta: NodeMeta::new(name.into(), desc.into()),
+      order: Vec::<Uuid>::new(),
     }
   }
 
-  pub fn get_node(&mut self, id: &Uuid) -> Option<&mut Node> {
-    self.items.get_mut(id)
+  fn add_order(&mut self, id: &Uuid) {
+    self.order.push(*id);
   }
 
-  pub fn add_node(&mut self, node: Node) {
-    let id = *node.id();
-    self.items.insert(id, node);
-    self.order.push(id);
-  }
-
-  pub fn remove_node(&mut self, id: &Uuid) {
-    self.items.remove(id);
+  fn remove_order(&mut self, id: &Uuid) {
     self.order.retain(|x| x != id);
   }
 
-  pub fn move_node(&mut self, id: &Uuid, new_pos: usize) {
-    if let Some(pos) = self.order.iter().position(|x| x == id) {
-      self.order.remove(pos);
-    }
-
-    self.order.insert(new_pos, *id);
+  fn move_order(&mut self, id: &Uuid, pos: usize) {
+    self.order.retain(|x| x != id);
+    self.order.insert(pos, *id);
   }
 
-  pub fn swap_nodes(&mut self, id1: &Uuid, id2: &Uuid) {
-    if let (Some(pos1), Some(pos2)) = (
+  fn swap_order(&mut self, id1: &Uuid, id2: &Uuid) {
+    if let (Some(i), Some(j)) = (
       self.order.iter().position(|x| x == id1),
-      self.order.iter().position(|y| y == id2)
+      self.order.iter().position(|y| y == id2),
     ) {
-      self.order[pos1] = *id2;
-      self.order[pos2] = *id1;
+      self.order.swap(i, j);
     }
   }
-
-  // {...}
 }
 
-impl HasMeta for Container {
-  fn meta(&self) -> &StructMeta {
-    &self.meta
+// Tree
+pub struct Tree {
+  root: Uuid,
+  nodes: HashMap<Uuid, Node>,
+  locations: HashMap<Uuid, Uuid>
+}
+
+impl Tree {
+  pub fn new(root: Container) -> Self {
+    let root_node = Node::Container(root);
+    let root_id = *root_node.id();
+
+    let mut nodes = HashMap::<Uuid, Node>::new();
+    nodes.insert(root_id, root_node);
+
+    Self {
+      root: root_id,
+      nodes,
+      locations: HashMap::<Uuid, Uuid>::new(),
+    }
   }
 
-  fn meta_mut(&mut self) -> &mut StructMeta {
-    &mut self.meta
+  // Utility
+  pub fn get_node(&mut self, node_id: &Uuid) -> Result<&mut Node, String> {
+    let node = self.nodes.get_mut(node_id)
+      .ok_or_else(|| format!("Node {} not found", node_id))?;
+
+    Ok(node)
+  }
+
+  pub fn get_entry(&mut self, node_id: &Uuid) -> Result<&mut Entry, String> {
+    let entry = self.get_node(node_id)?
+      .get_entry()
+      .ok_or_else(|| format!("Node {} must be an entry", node_id))?;
+
+    Ok(entry)
+  }
+
+  fn get_container(&mut self, node_id: &Uuid) -> Result<&mut Container, String> {
+    let container = self.get_node(node_id)?
+      .get_container()
+      .ok_or_else(|| format!("Node {} must be a container", node_id))?;
+
+    Ok(container)
+  }
+
+  pub fn get_parent_id(&self, node_id: &Uuid) -> Result<&Uuid, String> {
+    if *node_id == self.root {Err("Root node doesn't have parent container")?}
+
+    let parent_id = self.locations.get(node_id)
+      .ok_or_else(|| "Parent id not found".to_string())?;
+
+    Ok(parent_id)
+  }
+
+  fn get_parent_container(&mut self, node_id: &Uuid) -> Result<&mut Container, String> {
+    let parent_id = *self.get_parent_id(node_id)?;
+
+    let container = self.get_container(&parent_id)?;
+    Ok(container)
+  }
+
+  fn compare_parents(&self, node_id1: &Uuid, node_id2: &Uuid) -> Result<bool, String> {
+    let parent_id1 = self.get_parent_id(node_id1)?;
+    let parent_id2 = self.get_parent_id(node_id2)?;
+
+    Ok(parent_id1 == parent_id2)
+  }
+
+  // --- The interesting part ---
+
+  // Basic Nodes operations
+  pub fn add_node(&mut self, parent_id: &Uuid, node: Node) -> Result<(), String> {
+    let node_id = *node.id();
+
+    let container = self.get_container(parent_id)?;
+    container.add_order(&node_id);
+
+    self.nodes.insert(node_id, node);
+    self.locations.insert(node_id, *parent_id);
+
+    Ok(())
+  }
+
+  pub fn remove_node(&mut self, node_id: &Uuid) -> Result<(), String> {
+    let container = self.get_parent_container(node_id)?;
+    container.remove_order(node_id);
+
+    self.nodes.remove(node_id);
+    self.locations.remove(node_id);
+
+    Ok(())
+  }
+
+  pub fn move_node(&mut self, node_id: &Uuid, new_pos: usize) -> Result<(), String> {
+    let container = self.get_parent_container(node_id)?;
+    container.move_order(node_id, new_pos);
+
+    Ok(())
+  }
+
+  pub fn swap_nodes(&mut self, node_id1: &Uuid, node_id2: &Uuid) -> Result<(), String> {
+    if !self.compare_parents(node_id1, node_id2)? {Err("Can only swap nodes with the same parent")?}
+    if node_id1 == node_id2 {Err("Cannot swap a node with itself")?}
+
+    let container = self.get_parent_container(node_id1)?;
+    container.swap_order(node_id1, node_id2);
+
+    Ok(())
+  }
+
+  pub fn change_parent(&mut self, new_parent_id: &Uuid, node_id: &Uuid) -> Result<(), String> {
+    let parent_id = self.get_parent_id(node_id)?;
+    if parent_id == new_parent_id {Err(format!("Node already is inside of container {}", parent_id))?}
+
+    {
+      let container = self.get_parent_container(node_id)?;
+      container.remove_order(node_id);
+    }
+
+    {
+      let new_container = self.get_container(new_parent_id)?;
+      new_container.add_order(node_id);
+    }
+
+    self.locations.remove(node_id);
+    self.locations.insert(*node_id, *new_parent_id);
+
+    Ok(())
+  }
+
+  pub fn get_children_ids(&mut self, parent_id: &Uuid) -> Result<Vec<&Uuid>, String> {
+    let container = self.get_container(parent_id)?;
+
+    let children = container.order.iter().collect();
+    Ok(children)
+  }
+
+  // Entry Node operation
+  pub fn entry_state(&mut self, node_id: &Uuid) -> Result<&EntryState, String> {
+    let entry = self.get_entry(node_id)?;
+
+    Ok(&entry.state)
+  }
+
+  pub fn set_entry_state(&mut self, node_id: &Uuid, state: &EntryState) -> Result<(), String> {
+    let entry = self.get_entry(node_id)?;
+
+    entry.state = state.clone();
+    Ok(())
+  }
+
+  pub fn entry_state_next(&mut self, node_id: &Uuid) -> Result<&EntryState, String> {
+    let entry = self.get_entry(node_id)?;
+
+    entry.state.next();
+
+    Ok(&entry.state)
+  }
+
+  pub fn entry_state_prev(&mut self, node_id: &Uuid) -> Result<&EntryState, String> {
+    let entry = self.get_entry(node_id)?;
+
+    entry.state.prev();
+
+    Ok(&entry.state)
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-use std::io;
-use crate::{
-  data::{Entry, HasMeta}
-};
-
 mod tui;
 mod data;
 
-fn main() -> io::Result<()> {
-  let entry = Entry::new("A", "B");
-  println!("{}", entry.id());
-
-  tui::draw()
-}
+fn main() {}
